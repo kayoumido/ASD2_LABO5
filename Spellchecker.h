2b9b523351d58b1936c7b21cf51fa4d6526ab0ce
Doran Kayoumi <dorankayoumi@gmail.com>
2020-01-10 22:57:58 +0100

    Light refactor

--- Spellchecker.h
+++ Spellchecker.h
@@ -17,7 +17,7 @@
 #include "Dictionary.h"
 #include "STLDictionary.h"
 
-template <typename Dict>
+template<typename Dict>
 class Spellchecker {
     Dict dict;
     std::string toCorrect;
@@ -29,8 +29,8 @@ class Spellchecker {
 
         // replace all the special chars by a space
         // as specified in the given, the ' is kept
-        std::replace_if(cleaned.begin(), cleaned.end(), [] (const char& c) {
-            return (std::ispunct(c) or c == '-') and c != '\'' ;
+        std::replace_if(cleaned.begin(), cleaned.end(), [](const char &c) {
+            return (std::ispunct(c) or c == '-') and c != '\'';
         }, ' '); // for some reason the static const doesn't work \o/
 
         std::transform(cleaned.begin(), cleaned.end(), cleaned.begin(), [](unsigned char c) {
@@ -62,7 +62,7 @@ public:
                 if (word.empty())
                     continue;
 
-                if (!dict.find(word)){
+                if (!dict.find(word)) {
                     std::cout << "*" << word << std::endl;
                     checkMisspelledWord(word);
                 }
@@ -71,17 +71,17 @@ public:
     }
 
 private:
-    std::vector<std::string> wroteAnExtraLetter(const std::string &initialWord, const STLDictionary &dict){
+    std::vector<std::string> extraLetter(const std::string &initialWord) {
         std::vector<std::string> wordsSuggestion;
         std::string prefix = "1:";
 
         // Loop through each letter of the inital word
-        for(int i = 0; i < initialWord.size(); ++i){
+        for (int i = 0; i < initialWord.size(); ++i) {
             // Generate the word without a letter at current i pos
             std::string cpy = initialWord;
             //cout << cpy.erase(i,1) << endl;
-            cpy.erase(i,1);
-            if(dict.find(cpy)){
+            cpy.erase(i, 1);
+            if (dict.find(cpy)) {
                 wordsSuggestion.push_back(cpy.insert(0, prefix));
             }
         }
@@ -89,20 +89,20 @@ private:
         return wordsSuggestion;
     }
 
-    std::vector<std::string> forgotALetter(const std::string &initialWord, const STLDictionary &dict){
+    std::vector<std::string> forgottenLetter(const std::string &initialWord) {
         std::vector<std::string> wordsSuggestion;
         std::string prefix = "2:";
 
         // Loop through each letter of the inital word
-        for(int i = 0; i <= initialWord.size(); ++i){
+        for (int i = 0; i <= initialWord.size(); ++i) {
             // Loop throuhgh each alphabet letter
-            for(char letterToAdd = 'a'; letterToAdd <= 'z'; ++letterToAdd){
+            for (char letterToAdd = 'a'; letterToAdd <= 'z'; ++letterToAdd) {
                 // Generate the word with an extra letter at current i pos
                 std::string cpy = initialWord;
                 std::string strLetter(1, letterToAdd);
                 //cout << cpy.insert(i, strLetter) << endl;
                 cpy.insert(i, strLetter);
-                if(dict.find(cpy)){
+                if (dict.find(cpy)) {
                     wordsSuggestion.push_back(cpy.insert(0, prefix));
                 }
             }
@@ -111,22 +111,22 @@ private:
         return wordsSuggestion;
     }
 
-    std::vector<std::string> misspelledALetter(const std::string& initialWord, const STLDictionary &dict){
+    std::vector<std::string> misspelled(const std::string &initialWord) {
         std::vector<std::string> wordsSuggestion;
         std::string prefix = "3:";
 
         // Loop through each letter of the inital word
-        for(int i = 0; i < initialWord.size(); ++i){
+        for (int i = 0; i < initialWord.size(); ++i) {
             // Loop throuhgh each alphabet letter
-            for(char letterToAdd = 'a'; letterToAdd <= 'z'; ++letterToAdd){
+            for (char letterToAdd = 'a'; letterToAdd <= 'z'; ++letterToAdd) {
                 // Generate the word with an extra letter in place of the current i pos letter
                 std::string cpy = initialWord;
                 std::string strLetter(1, letterToAdd); // Cast letter in string
                 // Erase the letter at the i pos and add the new one
-                cpy.erase(i,1);
+                cpy.erase(i, 1);
                 cpy.insert(i, strLetter);
 
-                if(dict.find(cpy)){
+                if (dict.find(cpy)) {
                     wordsSuggestion.push_back(cpy.insert(0, prefix));
                 }
             }
@@ -135,17 +135,17 @@ private:
         return wordsSuggestion;
     }
 
-    std::vector<std::string> swappedTwoLetters(const std::string& initialWord, const STLDictionary &dict){
+    std::vector<std::string> swappedTwoLetters(const std::string &initialWord) {
         std::vector<std::string> wordsSuggestion;
         std::string prefix = "4:";
 
         // Loop through each letter of the inital word
-        for(int i = 0; i < initialWord.size() - 1; ++i){
+        for (int i = 0; i < initialWord.size() - 1; ++i) {
             // Generate the word with a swap between letter at pos i and i+1
             std::string cpy = initialWord;
-            std::swap(cpy[i], cpy[i+1]);
+            std::swap(cpy[i], cpy[i + 1]);
 
-            if(dict.find(cpy)){
+            if (dict.find(cpy)) {
                 wordsSuggestion.push_back(cpy.insert(0, prefix));
             }
         }
@@ -153,18 +153,24 @@ private:
         return wordsSuggestion;
     }
 
-
-    std::vector<std::string> checkMisspelledWord(const std::string &word, const STLDictionary &dict) {
-        std::vector<std::string> a = wroteAnExtraLetter(word, dict);
-        std::vector<std::string> b = forgotALetter(word, dict);
-        std::vector<std::string> c = misspelledALetter(word, dict);
-        std::vector<std::string> d = swappedTwoLetters(word, dict);
+    std::vector<std::string> checkMisspelledWord(const std::string &word) {
+        std::vector<std::string> a = extraLetter(word);
+        std::vector<std::string> b = forgottenLetter(word);
+        std::vector<std::string> c = misspelled(word);
+        std::vector<std::string> d = swappedTwoLetters(word);
 
         std::vector<std::string> suggestions;
         suggestions.insert(suggestions.begin(), a.begin(), a.end());
         suggestions.insert(suggestions.end(), b.begin(), b.end());
         suggestions.insert(suggestions.end(), c.begin(), c.end());
         suggestions.insert(suggestions.end(), d.begin(), d.end());
+
+        // TODO REM
+        // DEBUG CODE
+        for (std::string sugg : suggestions) {
+            std::cout << sugg << std::endl;
+        }
+        std::cout << std::endl;
 
         return suggestions;
     }
