6565b18010b92e8041db752694a0986ef4a0d9ea
Doran Kayoumi <dorankayoumi@gmail.com>
2020-01-10 15:59:01 +0100

    Add constructor

--- Spellchecker.h
+++ Spellchecker.h
@@ -5,9 +5,21 @@
 #ifndef LABO05_SPELLCHECKER_H
 #define LABO05_SPELLCHECKER_H
 
+#include <string>
+#include <utility>
+
+#include "DictionaryCommon.h"
+#include "STLDictionary.h"
 
 class Spellchecker {
-
+private:
+    DictionaryCommon *dict;
+    std::string toCorrect;
+public:
+    Spellchecker(std::string toCorrect, bool useSTL) : toCorrect(std::move(toCorrect)) {
+        STLDictionary STLDict("data/dictionary");
+        dict = &STLDict;
+    }
 };
 
 
