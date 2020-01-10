b747d4fff3298dbc34ea0193a7610bca211209a2
Doran Kayoumi <dorankayoumi@gmail.com>
2020-01-10 16:36:07 +0100

    Start correct method

--- Spellchecker.h
+++ Spellchecker.h
@@ -7,6 +7,8 @@
 
 #include <string>
 #include <utility>
+#include <fstream>
+#include <algorithm>
 
 #include "DictionaryCommon.h"
 #include "STLDictionary.h"
@@ -15,10 +17,27 @@ class Spellchecker {
 private:
     DictionaryCommon *dict;
     std::string toCorrect;
+
 public:
     Spellchecker(std::string toCorrect, bool useSTL) : toCorrect(std::move(toCorrect)) {
         STLDictionary STLDict("data/dictionary");
         dict = &STLDict;
+    }
+
+    void correct() {
+        std::ifstream s(this->toCorrect);
+
+        if (!s.is_open())
+            throw std::runtime_error("Unable to open file");
+
+        std::string line;
+        while (getline(s, line)) {
+            std::replace_if(line.begin(), line.end(),[] (const char& c) {
+                return std::ispunct(c) and c != '\'' ;
+            },' ');
+
+            
+        }
     }
 };
 
