521fb93d1468cd033a61dac7f05eb74d3e120b7c
Doran Kayoumi <dorankayoumi@gmail.com>
2020-01-10 22:58:18 +0100

    Add test code

--- main.cpp
+++ main.cpp
@@ -2,12 +2,25 @@
 #include <string>
 #include <fstream>
 #include <vector>
-#include <STLDictionary.h>
+#include "STLDictionary.h"
+#include "TernarySearchTrie.h"
+#include "Spellchecker.h"
+
+#define USE_STL 1
 
 using namespace std;
 
 int main() {
 
+#if USE_STL
+    STLDictionary stldict("data/dictionary.txt");
+    Spellchecker<STLDictionary> sc("data/input_lates.txt", stldict);
+#else
+    TernarySearchTrie tst("data/dictionary.txt");
+    Spellchecker<TernarySearchTrie> sc("data/input_lates.txt", tst);
+#endif
+
+    sc.correct();
 
     return 0;
 }
