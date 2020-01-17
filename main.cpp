5e65a5bf7ef9759d568514d1f910d110150cbbb4
Doran Kayoumi <dorankayoumi@gmail.com>
2020-01-17 16:35:29 +0100

    Add time display

--- main.cpp
+++ main.cpp
@@ -5,22 +5,41 @@
 #include "STLDictionary.h"
 #include "TernarySearchTrie.h"
 #include "Spellchecker.h"
+#include <chrono>
 
 #define USE_STL 1
 
 using namespace std;
 
 int main() {
+    chrono::high_resolution_clock::time_point start;
+    chrono::high_resolution_clock::duration dictLoadTime;
+    chrono::high_resolution_clock::duration correctionTime;
+
+    string fileToCorrect = "input_wikipedia.txt";
+    string dict = "dictionary.txt";
 
 #if USE_STL
-    STLDictionary stldict("data/dictionary.txt");
-    Spellchecker<STLDictionary> sc("data/input_wikipedia.txt", stldict);
+    start = chrono::high_resolution_clock::now();
+    STLDictionary stldict("data/" + dict);
+    dictLoadTime = chrono::high_resolution_clock::now() - start;
+
+    Spellchecker<STLDictionary> sc("data/" + fileToCorrect, stldict);
 #else
-    TernarySearchTrie tst("data/dictionary.txt");
-    Spellchecker<TernarySearchTrie> sc("data/input_lates.txt", tst);
+    start = chrono::high_resolution_clock::now();
+    TernarySearchTrie tst("data/" + dict);
+    dictLoadTime = chrono::high_resolution_clock::now() - start;
+    
+    Spellchecker<TernarySearchTrie> sc("data/" + fileToCorrect, tst);
 #endif
 
+    start = chrono::high_resolution_clock::now();
     sc.correct();
+    correctionTime = chrono::high_resolution_clock::now() - start;
+
+    cout << "Correcting : " << fileToCorrect << endl;
+    cout << "Dictionary load time : " << chrono::duration_cast<chrono::milliseconds>(dictLoadTime).count() << "ms" << endl;
+    cout << "Correction time : " << chrono::duration_cast<chrono::milliseconds>(correctionTime).count() << "ms" << endl;
 
     return 0;
 }
